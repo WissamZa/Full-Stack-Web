f54a5df04250df8b81662d39b55e8f389a8f7d5f
WissamZa <wzaidi@stu.kau.edu.sa>
2021-07-17 18:05:32 +0300

    React

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class CreateRommPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>Test CreateRommPage</p>;
+  }
+}
